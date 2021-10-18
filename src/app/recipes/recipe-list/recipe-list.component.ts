a975cc15eed11da7dd57449a74fcbf9e2da32848
Andrii Popenko <andrii.popenko@dbschenker.com>
2021-10-18 19:59:23 +0200

    navigation

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,6 +11,10 @@ export class RecipeListComponent implements OnInit {
     new RecipeModel(
       'A test recipe',
       'This is simply a test',
+      'https://www.simplyrecipes.com/thmb/ObstB0aXAmOO7sbsMIN7hixgD0Y=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2019__05__Chicken-Korma-LEAD-1-68475a749cd646b89137c9f6fddf6122.jpg'),
+    new RecipeModel(
+      'A test recipe',
+      'This is simply a test',
       'https://www.simplyrecipes.com/thmb/ObstB0aXAmOO7sbsMIN7hixgD0Y=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2019__05__Chicken-Korma-LEAD-1-68475a749cd646b89137c9f6fddf6122.jpg')
   ];
 

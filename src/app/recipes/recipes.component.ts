aad492a673b9f57b107d20b6410d3bc51e5adec5
AndrewPopenko <andrew.popenko@gmail.com>
2022-08-21 23:16:46 +0200

    use rxjs instead of events

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,23 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-import { Recipe } from "./recipe";
-import { RecipeService } from "../service/recipe.service";
+import {Component, OnInit} from '@angular/core';
+import {RecipeService} from "../service/recipe.service";
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css'],
-  providers: [RecipeService]
+  providers: [RecipeService],
 })
 export class RecipesComponent implements OnInit {
-  selectedRecipe: Recipe | undefined;
-
-  constructor(private recipeService: RecipeService) {}
+  constructor() {
+  }
 
   ngOnInit(): void {
-    this.recipeService.recipeSelected.subscribe(
-      (recipe: Recipe) => {
-        this.selectedRecipe = recipe;
-      }
-    )
   }
 }

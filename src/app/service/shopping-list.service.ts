fc680afabbd86f2a18253982c4e505e6907983c3
AndrewPopenko <andrew.popenko@gmail.com>
2023-01-31 00:13:26 +0100

    TD forms

--- src/app/service/shopping-list.service.ts
+++ src/app/service/shopping-list.service.ts
@@ -2,7 +2,8 @@ import { Ingredient } from "../shared/ingredient.model";
 import {Subject} from "rxjs";
 
 export class ShoppingListService {
-  ingredientsList: Subject<Ingredient[]> = new Subject<Ingredient[]>();
+  ingredientsChanged: Subject<Ingredient[]> = new Subject<Ingredient[]>();
+  startedEditing: Subject<number> = new Subject<number>();
   private ingredients: Ingredient[] = [
     new Ingredient('Apple', 5),
     new Ingredient('Tomatoes', 10)
@@ -14,11 +15,25 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
-    this.ingredientsList.next(this.getIngredients());
+    this.ingredientsChanged.next(this.getIngredients());
   }
 
   addIngredients(ingredients: Ingredient[]) {
     this.ingredients.push(...ingredients);
-    this.ingredientsList.next(this.getIngredients());
+    this.ingredientsChanged.next(this.getIngredients());
+  }
+
+  getIngredient(index: number): Ingredient {
+    return this.ingredients[index];
+  }
+
+  updateIngredient(index: number, newIngredient: Ingredient): void {
+    this.ingredients[index] = newIngredient;
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  deleteIngredient(index: number): void {
+    this.ingredients.splice(index, 1);
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 }

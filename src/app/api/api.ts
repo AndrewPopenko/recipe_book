3658b8e2c2722979062fbfc4176a17f6c51a1e9d
AndrewPopenko <andrew.popenko@gmail.com>
2023-10-14 14:47:31 +0200

    authentication

--- src/app/api/api.ts
+++ src/app/api/api.ts
@@ -4,7 +4,6 @@ export class Api {
   static readonly signup = Api.authUrl + Api.apiVersion + '/accounts:signUp';
   static readonly signin =
     Api.authUrl + Api.apiVersion + '/accounts:signInWithPassword';
-  static readonly key = 'AIzaSyDc-ZRzBF7KxP9R6P36EK0g50WIoj7NqNg';
   static readonly baseUrl =
     'https://ng-course-recipe-book-79a0e-default-rtdb.europe-west1.firebasedatabase.app';
 }
